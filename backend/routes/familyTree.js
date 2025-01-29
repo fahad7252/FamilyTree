968bb5d36a916e09a8a7ed545e15406e1b235dce
Fahad Hossain <skr_fah@hotmail.com>
2025-01-29 02:55:10 -0500

    gotta rework on the back end and services it didnt work

--- backend/routes/familyTree.js
+++ backend/routes/familyTree.js
@@ -1,13 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const familyTreeCtrl = require("../controllers/familyTree");
 
-router.get("/", familyTreeCtrl.getAllMembers);
-router.get("/:id", familyTreeCtrl.getMember);
-router.post("/", familyTreeCtrl.createMember);
-router.put("/:id", familyTreeCtrl.updateMember);
-router.delete("/:id", familyTreeCtrl.deleteMember);
-
-router.put("/:id/details", familyTreeCtrl.updateMemberDetails);
-
-module.exports = router;
--- frontend/src/services/familyTreeService.js
+++ frontend/src/services/familyTreeService.js
@@ -1,33 +1,3 @@
 import sendRequest from "./sendRequest";
 
 const BASE_URL = "/api/familyTree";
-
-// get all members
-export function getAllMembers() {
-  return sendRequest(BASE_URL);
-}
-
-// SINGLE member
-export function getMember(id) {
-  return sendRequest(`${BASE_URL}/${id}`);
-}
-//new member
-export function createMember(memberData) {
-  return sendRequest(BASE_URL, "POST", memberData);
-}
-// update  member
-export function updateMember(id, memberData) {
-  return sendRequest(`${BASE_URL}/${id}`, "PUT", memberData);
-}
-
-export function deleteMember(id) {
-  return sendRequest(`${BASE_URL}/${id}`, "DELETE");
-}
-
-export function updateMemberDetails(id, details) {
-  return sendRequest(`${BASE_URL}/${id}/details`, "PUT", details);
-}
-
-export function updateMemberPosition(id, position) {
-  return sendRequest(`${BASE_URL}/${id}`, "PUT", { position });
-}
